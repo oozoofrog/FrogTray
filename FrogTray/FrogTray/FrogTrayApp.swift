302dc34187704ad9b7fe29370918a9d3eb73bbe2
oozoofrog <oozoofrog@gmail.com>
2026-03-07 00:35:01 +0900

    Initial FrogTray app scaffold

---
+++ FrogTray/FrogTray/FrogTrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrogTrayApp.swift
+//  FrogTray
+//
+//  Created by oozoofrog on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct FrogTrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FrogTray/FrogTrayTests/FrogTrayTests.swift
@@ -0,0 +1,17 @@
+//
+//  FrogTrayTests.swift
+//  FrogTrayTests
+//
+//  Created by oozoofrog on 3/7/26.
+//
+
+import Testing
+@testable import FrogTray
+
+struct FrogTrayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
